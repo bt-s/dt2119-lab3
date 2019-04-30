3547081d4d30bcf5720d9536f1dcc6a05ced714f
Pietro Alovisi <alovisi@kth.se>
2019-04-30 17:03:22 +0200

    _

---
+++ lab3_dnn.py
@@ -0,0 +1,15 @@
+import numpy as np
+from lab3_tools import *
+
+
+phoneHMMs = np.load('lab2_models_all.npz')['phoneHMMs'].item()
+phones = sorted(phoneHMMs.keys())
+nstates = {phone: phoneHMMs[phone]['means'].shape[0] for phone in phones}
+stateList = [ph + '_' + str(id) for ph in phones for id in range(nstates[ph])]
+
+
+print(stateList)
+
+filename = 'tidigits/disc_4.1.1/tidigits/train/man/nw/z43a.wav'
+samples, samplingrate = loadAudio(filename)
+lmfcc = mfcc(samples)
